930737ed61dde3787bb7738a293afd0028a9f49d
ibrihem99 <ibriheemm176@gmail.com>
2025-04-11 19:45:14 +0300

    Add files via upload

---
+++ lawertik/script.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("subscriptionForm");
+  const confirmationMessage = document.getElementById("confirmationMessage");
+  
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    // هنا يمكن إضافة منطق لإرسال البيانات إلى خادم أو قاعدة بيانات
+    confirmationMessage.classList.remove("hidden");
+    form.reset();
+  });
+});
